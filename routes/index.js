b1a8234f46ebed6b1df9ae985011fa5a7cee5752
DavideFranzys <webmaster@franzysonline.it>
2025-02-12 10:09:15 +0100

    emailfix1

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer');
 //app.use(bodyParser());
 
 let transport = {
-  host: "smtp.aruba.it",
+  host: "smtps.aruba.it",
     port: 465,
     secure: true,
     auth: {
